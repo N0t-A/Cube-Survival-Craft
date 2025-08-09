7c6fe5d388cbd98bad40ffae1a30a620eb0e9387
N0t-A <andrew.j.portteus@gmail.com>
2025-08-09 08:25:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ function generateFlatWorld() {
       block.className = 'grass block';
       const posX = x * blockSize;
       const posZ = z * blockSize;
-      const posY = groundY - 700;
+      const posY = groundY;
       block.style.transform = `translate3d(${posX}px, ${posY}px, ${posZ}px)`;
 
       createBlockFaces(block);
