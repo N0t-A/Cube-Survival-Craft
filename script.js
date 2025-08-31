35210d245c7fd08e7bf35375318e850073a6f09f
N0t-A <andrew.j.portteus@gmail.com>
2025-08-31 14:26:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1973,7 +1973,7 @@ function updatePlayerPosition() {
  const surfaceY = getTopSurfaceYUnderPlayer();
 const feetY = posY - characterYOffset;
 
-if (surfaceY !== undefined && feetY < surfaceY) { 
+if (surfaceY !== undefined && feetY > surfaceY) { 
   // Feet are below the surface
   posY = surfaceY + characterYOffset;
   velY = 0;
